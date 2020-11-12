e6592ae3508aa01c0751f8a5ea77cfffaf48aff9
iviczian <viczian.istvan@gmail.com>
2020-11-12 23:43:21 +0100

    Index rontás

--- solutions/finalmodifier/src/main/java/finalmodifier/Week.java
+++ solutions/finalmodifier/src/main/java/finalmodifier/Week.java
@@ -8,7 +8,7 @@ public class Week {
     public static final List<String> DAYS = Arrays.asList("Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap");
 
     public static void main(String[] args) {
-        DAYS.set(2, "Szerda");
+        DAYS.set(1, "Szerda");
         System.out.println(DAYS);
     }
 }
